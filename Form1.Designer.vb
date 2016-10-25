895e4422e97a88b37bcf0b4360c41394789a3dca
pppeedrito <pedro1996616@gmail.com>
2016-10-25 17:12:37 +0200

    repositorio actualizado

---
+++ Form1.Designer.vb
@@ -0,0 +1,286 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txt1 = New System.Windows.Forms.Label()
+        Me.txt2 = New System.Windows.Forms.Label()
+        Me.txt3 = New System.Windows.Forms.Label()
+        Me.txt4 = New System.Windows.Forms.Label()
+        Me.txt5 = New System.Windows.Forms.Label()
+        Me.txt6 = New System.Windows.Forms.Label()
+        Me.txt7 = New System.Windows.Forms.Label()
+        Me.txt8 = New System.Windows.Forms.Label()
+        Me.txtDivisor = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txt1
+        '
+        Me.txt1.AutoSize = True
+        Me.txt1.Location = New System.Drawing.Point(28, 59)
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(51, 17)
+        Me.txt1.TabIndex = 0
+        Me.txt1.Text = "Label1"
+        '
+        'txt2
+        '
+        Me.txt2.AutoSize = True
+        Me.txt2.Location = New System.Drawing.Point(28, 118)
+        Me.txt2.Name = "txt2"
+        Me.txt2.Size = New System.Drawing.Size(51, 17)
+        Me.txt2.TabIndex = 1
+        Me.txt2.Text = "Label2"
+        '
+        'txt3
+        '
+        Me.txt3.AutoSize = True
+        Me.txt3.Location = New System.Drawing.Point(28, 162)
+        Me.txt3.Name = "txt3"
+        Me.txt3.Size = New System.Drawing.Size(51, 17)
+        Me.txt3.TabIndex = 2
+        Me.txt3.Text = "Label3"
+        '
+        'txt4
+        '
+        Me.txt4.AutoSize = True
+        Me.txt4.Location = New System.Drawing.Point(28, 202)
+        Me.txt4.Name = "txt4"
+        Me.txt4.Size = New System.Drawing.Size(51, 17)
+        Me.txt4.TabIndex = 3
+        Me.txt4.Text = "Label4"
+        '
+        'txt5
+        '
+        Me.txt5.AutoSize = True
+        Me.txt5.Location = New System.Drawing.Point(28, 256)
+        Me.txt5.Name = "txt5"
+        Me.txt5.Size = New System.Drawing.Size(51, 17)
+        Me.txt5.TabIndex = 4
+        Me.txt5.Text = "Label5"
+        '
+        'txt6
+        '
+        Me.txt6.AutoSize = True
+        Me.txt6.Location = New System.Drawing.Point(28, 304)
+        Me.txt6.Name = "txt6"
+        Me.txt6.Size = New System.Drawing.Size(51, 17)
+        Me.txt6.TabIndex = 5
+        Me.txt6.Text = "Label6"
+        '
+        'txt7
+        '
+        Me.txt7.AutoSize = True
+        Me.txt7.Location = New System.Drawing.Point(28, 365)
+        Me.txt7.Name = "txt7"
+        Me.txt7.Size = New System.Drawing.Size(51, 17)
+        Me.txt7.TabIndex = 6
+        Me.txt7.Text = "Label7"
+        '
+        'txt8
+        '
+        Me.txt8.AutoSize = True
+        Me.txt8.Location = New System.Drawing.Point(28, 410)
+        Me.txt8.Name = "txt8"
+        Me.txt8.Size = New System.Drawing.Size(51, 17)
+        Me.txt8.TabIndex = 7
+        Me.txt8.Text = "Label8"
+        '
+        'txtDivisor
+        '
+        Me.txtDivisor.AutoSize = True
+        Me.txtDivisor.Location = New System.Drawing.Point(297, 451)
+        Me.txtDivisor.Name = "txtDivisor"
+        Me.txtDivisor.Size = New System.Drawing.Size(51, 17)
+        Me.txtDivisor.TabIndex = 8
+        Me.txtDivisor.Text = "Label9"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(108, 54)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox1.TabIndex = 9
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(108, 118)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox2.TabIndex = 10
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(108, 162)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox4.TabIndex = 12
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(108, 202)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox5.TabIndex = 13
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(108, 253)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox6.TabIndex = 14
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(108, 304)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox7.TabIndex = 15
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(108, 355)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox8.TabIndex = 16
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(108, 410)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox9.TabIndex = 17
+        '
+        'TextBox10
+        '
+        Me.TextBox10.Location = New System.Drawing.Point(426, 446)
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox10.TabIndex = 18
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(511, 95)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 19
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(511, 156)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 20
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(511, 230)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 21
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(511, 298)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 22
+        Me.Button4.Text = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1063, 701)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox10)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtDivisor)
+        Me.Controls.Add(Me.txt8)
+        Me.Controls.Add(Me.txt7)
+        Me.Controls.Add(Me.txt6)
+        Me.Controls.Add(Me.txt5)
+        Me.Controls.Add(Me.txt4)
+        Me.Controls.Add(Me.txt3)
+        Me.Controls.Add(Me.txt2)
+        Me.Controls.Add(Me.txt1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txt1 As Label
+    Friend WithEvents txt2 As Label
+    Friend WithEvents txt3 As Label
+    Friend WithEvents txt4 As Label
+    Friend WithEvents txt5 As Label
+    Friend WithEvents txt6 As Label
+    Friend WithEvents txt7 As Label
+    Friend WithEvents txt8 As Label
+    Friend WithEvents txtDivisor As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles txt5.Click
+
+    End Sub
+
+    Private Sub txt6_Click(sender As Object, e As EventArgs) Handles txt6.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label9_Click(sender As Object, e As EventArgs) Handles txtDivisor.Click
+
+    End Sub
+End Class
